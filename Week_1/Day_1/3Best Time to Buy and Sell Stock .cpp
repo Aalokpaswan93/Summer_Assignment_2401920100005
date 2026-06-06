e4b193afc22d2d6aef033fbbe12a79114d404395
Aalokpaswan93 <aalokpaswan93@gmail.com>
2026-06-07 01:31:32 +0530

    Day_1

---
+++ Week_1/Day_1/3Best Time to Buy and Sell Stock .cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = prices[0];
+        int profit = 0;
+
+        for (int price : prices) {
+            buy = min(buy, price);
+            profit = max(profit, price - buy);
+        }
+
+        return profit;
+    }
+};
