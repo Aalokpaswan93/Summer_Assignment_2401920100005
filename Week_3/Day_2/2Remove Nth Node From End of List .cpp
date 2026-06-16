1ae9c1fbd26ea65866b52044d8b54b420f8b87bd
Aalokpaswan93 <aalokpaswan93@gmail.com>
2026-06-16 15:11:57 +0530

    W3Day_2

---
+++ Week_3/Day_2/2Remove Nth Node From End of List .cpp
@@ -0,0 +1,17 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *fast = head, *slow = head;
+        for (int i = 0; i < n; i++) fast = fast->next;
+        if (!fast) return head->next;
+        while (fast->next) fast = fast->next, slow = slow->next;
+        slow->next = slow->next->next;
+        return head;
+    }
+};
