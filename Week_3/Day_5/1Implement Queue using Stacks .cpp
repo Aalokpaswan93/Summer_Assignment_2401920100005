66d3c60ef2fd57b14018bc5c63ed16d9038d82a4
Aalokpaswan93 <aalokpaswan93@gmail.com>
2026-06-19 14:13:29 +0530

    W3Day_5

---
+++ Week_3/Day_5/1Implement Queue using Stacks .cpp
@@ -0,0 +1,37 @@
+#include <stack>
+using namespace std;
+class MyQueue {
+public:
+    stack<int>in;
+    stack<int>out;   
+    
+    void push(int x) {
+        in.push(x);
+    }
+    
+    int pop() {
+        if(out.empty())
+            while(!in.empty())
+            {
+                out.push(in.top());
+                in.pop();
+            }
+        int top=out.top();
+        out.pop();
+        return top;
+    }
+    
+    int peek() {
+        if(out.empty())   
+            while(!in.empty())
+            {
+                out.push(in.top());
+                in.pop();
+            }
+        return out.top();
+    }
+    
+    bool empty() {
+        return (in.empty() && out.empty());
+    }
+};
